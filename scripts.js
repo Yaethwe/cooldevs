e00e7f5e1c8147bb04c977aaae0dfa2b5566996d
Yeaethwe <76844276+Yaethwe@users.noreply.github.com>
2022-01-18 15:14:37 +0630

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,102 +1,91 @@
-let x;
-function SignUp() {
-    document.getElementById("SignUpBox").className="signupBox";
+
+let users;
+//const url = 'https://yaethwe.github.io/YT/users.json';
+
+/*
+async function getData() {
+	const response = await fetch(url);
+	const data = await response.json();
+	users=data;
+}
+getData();
+*/
+const file001 = "JSTextEditor.zip";
+const app001 = `
+		<div class="projectDiv">
+			<h3>  JS Text Editor  </h3>
+			<p>This project is a simple textEditor in HTML/CSS&JavaScript.</p>
+			<p>And this text editor is open source. You can copy my project.</p>
+			<p>Click the download button to download the project zip free.</p>
+			<button class="downloadbtn" onclick="download('JSTextEditor')">DOWNLOAD</button>
+		</div>
+		`;
+
+const app002 = `
+		<div class="projectDiv">
+			<center>
+			<img src="https://nyankaungset.github.io/test4test/Language.ico" class="ico"><h3>  test4test Language </h3>
+			</center>
+			<p>This is a Burmese Grammer Website project.</p>
+			<p>I don't know it is opensource or not.</p>
+			<p>I can't let you to download that project but you can view it.</p>
+			<p><span class="link"><a href="https://nyankaungset.github.io/test4test/" target="_blank">Click Here</a></span>to go to that website.</p>
+			<p>This project was created by Ko.NyanKaungSet</p>
+
+		</div>
+		`;
+
+const app003 = `
+		<div class="projectDiv">
+			<center>
+			<img src="https://nyankaungset.github.io/fresh-mobile-market.com/mmdish3.jpg" class="ico"><h3>  Fresh Mobile Market </h3>
+			</center>
+			<p>This is a Online Shopping Website project.</p>
+			<p>I don't know it is opensource or not.</p>
+			<p>I can't let you to download that project but you can view it.</p>
+			<p><span class="link"><a href="https://nyankaungset.github.io/fresh-mobile-market.com/" target="_blank">Click Here</a></span>to go to that website.
+			<p>This project was created by Ko.NyanKaungSet</p>
+		</div>
+		`;
+		
+function closeLogIn() {
+	logDiv.style="display:none";
 }
 
-function course() {
-    document.getElementById("course").className="videoDiv";	
+function reload() {
+	location.reload();
 }
 
-function closecourse() {
-	document.getElementById("course").className="hide";
-}
-    
-function LogIn() {
-    document.getElementById("logInBox").className="loginBox";
-}
-
-function pgc() {
-	document.getElementById("PGCL").className="videoDiv";
-	document.getElementById("image1").className="videotitle";
-}
-
-function introhtml() {
-	document.getElementById("introhtml").className="videoDiv";
-}
-
-function introjs() {
-	document.getElementById("introjs").className="videoDiv";
-}
-
-function notfound() {
-	document.getElementById("notfound").className="videoDiv";
-}
-
-function box01() {
-	document.getElementById("box01").className="defbox";
-}
-
-
-
-function search() {
-    x = document.getElementById("searchbox").value;
-	if (x=="PGC") {
-		pgc();
-	}else if (x=="PGC logo") {
-		pgc();
-	}else if (x=="html intro") {
-		introhtml();
-	}else if (x=="html") {
-		introhtml();
-	}else if (x=="introduction of html") {
-		introhtml();
-	}else if (x=="course") {
-		course();
-	}else if (x=="javascript") {
-		introjs();
+function download(file) {
+	if (file=="JSTextEditor") {
+		location.href=file001;
 	}
-	 else {
-		notfound();
+}
+
+function project(name) {
+	if (name=='JSTextEditor') {
+		document.getElementById("middleDiv").innerHTML=app001;
+	}else if (name=='test4test') {
+		document.getElementById("middleDiv").innerHTML=app002;
 	}
 }
-    
-function hide() {
-    document.getElementById("logInBox").className="hide";
-}
-    
-function hide1() {
-    document.getElementById("SignUpBox").className="hide";
-}
 
-function createAccount() {
-	username = document.getElementById("signupUsername").value;
-	password = document.getElementById("signupPassword").value;
-	email = document.getElementById("signupEmail").value;
-	phone = document.getElementById("signupPhonenumber").value;
+function logIn(ue, ps) {
+	for (let i = 0; i<1; i++) {
+		if (up[i].name+up[i].pass==ue+ps) {
+			logDiv.style="display:none;";
+			logined(ue, ps, up[i].level, up[i].photo, up[i].gender);
+			break;
+		}
+	}
 }
 
-
-
-function closepgcl() {
-	document.getElementById("PGCL").className="hide";
+function logInBtn() {
+	logIn(un.value, pd.value);
 }
 
-function closehtmlintro() {
-	document.getElementById("introhtml").className="hide";
+function openLogIn(){ 
+	logDiv.style="display:flex;";
 }
+closeLogIn();
 
-function closejsintro() {
-	document.getElementById("introjs").className="hide";
-}
-
-function closenotfound() {
-	document.getElementById("notfound").className="hide";
-}
-
-function closecourse() {
-	document.getElementById("course").className="hide";
-}
-
-function closebox01() {
-	document.getElementById("box01").className="hide";
-}
