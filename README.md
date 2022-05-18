442ce8bc002cf950db1790fad293855bcaa43c66
Yeaethwe <76844276+Yaethwe@users.noreply.github.com>
2022-05-18 09:48:24 +0630

    decorated version
    Version 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+![apple](img/fav.png)
+ # Cool-Devs
+
+|
+[YeaeThawe](https://yeaethawe.netlify.app/)
+--- 
+|
+[NyanKaungSet](https://nyankaungset.netlify.app)
+---
+___
+
+```javascript
+/*
+I don't know about Mark Down Language very much. If you know please explain me about that.
+  by YeaeThawe 5/17/2022 5:57PM Myanmar
+*/
+```
---
+++ js/index.js
@@ -0,0 +1,51 @@
+const body = document.querySelector('body');
+
+const msgDiv = document.querySelector('#msgDiv');
+const loginA = document.querySelector('#login-a');
+const signupA = document.querySelector('#signup-a');
+const logoutA = document.querySelector('#logout-a');
+
+const msgArray = [];
+
+class Message {
+    constructor(text,style){
+        this.text= text;
+        this.style= style;
+        this.popup();
+    }
+    popup(){
+        this.msgBox = document.createElement('div');
+        this.msgBox.className = this.style;
+        this.msg = document.createElement('p');
+        this.xBtn = document.createElement('a');
+
+        this.msg.innerHTML = this.text;
+
+        this.xBtn.className = "xBtn rounded pad-mar";
+        this.xBtn.setAttribute("title","Click to close.");
+        this.xBtn.innerHTML = "x";
+
+        this.msgBox.appendChild(this.msg);
+        this.msgBox.appendChild(this.xBtn);
+        msgDiv.appendChild(this.msgBox);
+        msgArray.push(this.msgBox);
+
+        this.xBtn.onclick=()=>{this.delete()}
+    }
+    delete(){
+        msgArray.splice(msgArray.indexOf(this.msgBox),1);
+        msgDiv.removeChild(this.msgBox);
+    }
+}
+
+let yt = new Message("This is a testing message box.", "msg-container-rounded bg-violet SS fg-white activeC");
+
+auth.onAuthStateChanged(user => {
+	if(user){
+        loginA.style.display='none';
+        signupA.style.display='none';
+        logoutA.style.display='flex';
+    }else{
+        logoutA.style.display='none';
+    }
+})
